9154a4daa722215f925b372d89b1c3db824c1576
unknown <you@example.com>
2022-03-11 20:19:37 +0300

    add content working code index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(braces, bracesMap) {
+  const findPair = (brace) => bracesMap.find((pair) => brace === pair[0])?.[1];
+  return !braces.split("").reduce((prev, cur) => {
+    if (cur === findPair(prev[prev.length - 1])) {
+      prev.pop();
+    } else {
+      prev.push(cur);
+    }
+    return prev;
+  }, []).length;
+};
